4f1192467c5374c75d3fafa8968454e70b8963c8
Selene Fleita <selene.f353@gmail.com>
2024-06-27 14:40:31 -0300

    se creo el servidor

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 const express = require(`express`)
 const morgan = require(`morgan`)
 const cors = require(`cors`)
+
+const app = express()
+const PORT = 3000
+
+app.use(express.json())
+app.use(morgan(`dev`))
+app.use(cors())
+
+app.listen(PORT, () =>{
+    console.log("Se inicio el servidor")
+} )
