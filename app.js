06840755fc78915dd48603baf7d8f13bae8eb21b
Selene Fleita <selene.f353@gmail.com>
2024-06-27 15:52:27 -0300

    se implementaron las rutas al trabajo

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require(`express`)
 const morgan = require(`morgan`)
 const cors = require(`cors`)
-
+const { productos } = require("./bd.js")
 const app = express()
 const PORT = 3000
 
@@ -9,6 +9,58 @@ app.use(express.json())
 app.use(morgan(`dev`))
 app.use(cors())
 
-app.listen(PORT, () =>{
+app.listen(PORT, () => {
     console.log("Se inicio el servidor")
-} )
+})
+
+//Devuelve la lista completa 
+
+app.get('/', (req, res) => {
+    res.json(productos)
+})
+
+//Devuelve un único producto según el id
+
+app.get('/products/:id', (req, res) => {
+    const { id } = req.params
+    const prod = productos.find(prod => prod.id == id)
+    res.json(prod)
+})
+
+//agregar un nuevo producto
+
+app.post('/products', (req, res) => {
+    const { nombre, precio } = req.body
+    const id = productos.length + 1
+    productos.push({ id, nombre, precio })
+    res.send('Se ha agregado un nuevo producto.')
+})
+
+//actualizar  producto
+
+app.put('/products/:id', (req, res) => {
+    const { nombre, precio } = req.body;
+    const id = req.params.id;
+    productos.forEach(prod => {
+        if (prod.id == id) {
+            prod.nombre = nombre;
+            prod.precio = precio;
+        }
+    });
+    res.send('Se actualizo correctamente')
+}) 
+
+//eliminar un producto por su id
+
+app.delete('/products/:id', (req, res) => {
+    const { id } = req.params
+    productos.forEach((prod, index) => {
+        if (prod.id == id) {
+            productos.splice(index, 1)
+        }
+    })
+    res.send('El producto fue eliminado.')
+})
+
+
+
---
+++ bd.js
@@ -0,0 +1,28 @@
+const productos = [
+    {
+        id: 1,
+        nombre: "gel capilar",
+        precio: 100.09
+    },
+    {
+        id: 2,
+        nombre: "shampoo",
+        precio: 399.99
+    },
+    {
+        id: 3,
+        nombre: "set tapers",
+        precio: 540.02
+    },
+    {
+        id: 4,
+        nombre: "papel",
+        precio: 10.02
+    }
+]
+module.exports = { productos }
+
+
+
+
+
